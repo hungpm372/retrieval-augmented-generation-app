040788e6b70abea6f69e834d107944e36a096bc3
hungpm372 <hungpm372@gmail.com>
2024-06-27 23:29:03 +0700

    RAG and conversational

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import os
 import bs4
 
 from dotenv import load_dotenv
-from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.chat_message_histories import ChatMessageHistory
 
 load_dotenv(dotenv_path=".env.dev")
 
-model = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 loader = WebBaseLoader(
     web_paths=("https://znews.vn/doi-bong-gay-soc-nhat-tai-vck-euro-2024-post1483303.html",),
@@ -31,28 +35,75 @@ splits = text_splitter.split_documents(docs)
 vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
 retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
 
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
-        | model
-        | StrOutputParser()
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "biết chết liền. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
 )
 
-# result = rag_chain.invoke("Đội nào thua?")
-# print(result)
+contextualize_q_system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "biết chết liền. Use three sentences maximum and keep the "
+    "answer concise."
+)
+
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
 
 while True:
     question = input("Enter your question: ")
 
     print("Answer: ", end="", flush=True)
 
-    for chunk in rag_chain.stream(question):
-        print(chunk, end="", flush=True)
+    response = conversational_rag_chain.invoke({"input": question}, config={
+        "configurable": {"session_id": "1"}
+    })
+
+    print(response["answer"], end="", flush=True)
+
     print()
